e660dcb170b94442dd7361175aea7eb2c8a3cb02
Eduardo Moura <eduardofreitasmoura@gmail.com>
2023-06-21 22:19:39 -0300

    Alterações no Predict.py
    Tamanho do modelo não corresponde com o do banco... RESOLVER!!!

--- backend/python_scripts/predict.py
+++ backend/python_scripts/predict.py
@@ -18,10 +18,13 @@ db = client['db']
 
 # Escolhendo coleção
 #if(sounding):
+#collection = db['soundings']
 #if(era5):
-#if(cor):
+#collection = db['era5']
+#if(cors):
+#collection = db['cors']
 #if(inmet):
-collection = db['soundings']
+collection = db['inmets']
 
 result = collection.find()
 documentosSounding = list(result)
@@ -30,31 +33,69 @@ df = pd.DataFrame(documentosSounding)
 
 client.close()
 
-print(df)
+#print(df)
 
-df['time'] = pd.to_datetime(df['time'])
-df = df.sort_values(by='time', ascending=False)
+# Ordenar pelos campos datas pelos 6 dados mais recentes.
+# Com intuito de organizar e como cada fonte de dados pode 
+# ter diferentes nomes para o campo data, foi separado a
+# ordenação por coleção.
+
+if collection == db['inmets']:
+    df['TIME'] = pd.to_datetime(df['TIME'])
+    df = df.sort_values(by='TIME', ascending=False)
+
+if collection == db['cors']:
+    df['data'] = pd.to_datetime(df['data'])
+    df = df.sort_values(by='data', ascending=False)
+
+if collection == db['era5']:
+    df['time'] = pd.to_datetime(df['time'])
+    df = df.sort_values(by='time', ascending=False)
+
+if collection == db['sounding']:
+    df['time'] = pd.to_datetime(df['time'])
+    df = df.sort_values(by='time', ascending=False)
+
+# Criação de um dataFrame com as seis linhas mais recentes para predição
+
 df_predict = df.head(6)
 df_predict = df_predict.reset_index(drop=True)
 print(df_predict)
 
-x = df_predict.iloc[:, :-1].values
+# Cada tabela no banco de dados vem com alguns campos indesejáveis, devido a forma que ela 
+# foi inserida provavelmente. Para cada tabela usamos o .iloc[:, 1:-1] para:
+# ler todas aslinhas ':'
+# começar após a primeira coluna '1'
+# terminar na penúltima coluna '-1'
+
+if collection == db['inmets']: x = df_predict.iloc[:, 1:-3].values
+if collection == db['cors']: x = df_predict.iloc[:, 1:-1].values
+if collection == db['era5']: x = df_predict.iloc[:, 1:].values
+if collection == db['sounding']: x = df_predict.iloc[:, 1:-1].values
+
 print(f"Shape of one test example: {x.shape}")
 print("Model input:")
 print(x)
 
 # Definir as variáveis NUM_FEATURES e NUM_CLASSES
+
 NUM_FEATURES = x.shape[1]
 NUM_CLASSES = 5
 
-#Create an instance of the model
+#C Cria uma instância do modelo
 model = OrdinalClassificationNet(in_channels=NUM_FEATURES, num_classes=NUM_CLASSES)
 
-# Load the pretrained model weights from the file
-model_path = "C:\\Users\\eduar\\Documents\\GitHub\\atmoseerPCS\\backend\\python_scripts\\best_A652_N_OC.pt" #Mudar caminho 
+# Carrega o modelo pré treinado e seus pesos do arquivo
+#diretorio_atual = os.getcwd()
+#pasta_predictModules = os.path.join(diretorio_atual, 'backend', 'predictModules')
+
+#model_path = pasta_predictModules + "\best_" + pipeline_id + "_OC.pt"
+#print(model_path)
+
+model_path = "C:\\Users\\eduar\\Documents\\GitHub\\atmoseerPCS\\backend\\predictModels\\best_A652_N_OC.pt" 
 model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 
-# Set the model in evaluation (inference) mode.
+# Colocar modelo no modo de avaliação (inferência).
 model.eval()
 
 # Make prediction using the loaded model
